ec7519e95dcbfdf8459b9349771d4592b46604fc
fanjiangfeng <fantongxue666@163.com>
2020-11-03 21:10:49 +0800

    新增Rain-License服务器证书模块

---
+++ rain-license/src/test/java/com/rain/license/rainlicense/RainLicenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rain.license.rainlicense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainLicenseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
